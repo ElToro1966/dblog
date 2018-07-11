e1170a6d5ec4b005d55ec8e8887eb9573912e9f5
ElToro1966 <mail@ericeikrem.com>
2018-07-11 17:29:47 +0200

    Added post create, edit and delete views, and corresponding tests.

--- blog/tests.py
+++ blog/tests.py
@@ -39,7 +39,32 @@ class PostModelTest(TestCase):
         response = self.client.get('/post/1/')
         no_response = self.client.get('/post/1000000/')
         self.assertEqual(response.status_code, 200)
-        self-assertEqual(no_response.status_code, 400)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertTemplateUsed(response, 'post_detail.html')
+        self.assertContains(response,'Blog post body')
+
+    def test_post_create_form_view(self):
+        response = self.client.get(reverse('post_new'),{
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+        self.assertContains(response, self.user)
+
+    def test_post_update_form_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Ipdated title',
+            'body': 'Updated body',
+            })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
 
     @skip
     def test_blog_post_on_admin_page(self):
