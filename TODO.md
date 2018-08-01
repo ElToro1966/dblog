51965faf6ea0d19e3f1548464c98ca56950dc4fb
ElToro1966 <mail@ericeikrem.com>
2018-08-01 10:04:39 +0200

    Added tests.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,8 @@
 ##TODO##
 
 Functional test -> user story
-Default user for author in Post-model
 Test on admin page with login. Selenium? TestCase?
-
+Separate test classes
 Separate test files
+FT: Test other browsers than Firefox.
+
--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,14 +14,36 @@ class NewVisitorTest(unittest.TestCase):
     def test_user_can_CRUD_a_post(self):
         # User enters blog web page
         self.browser.get('http://localhost:8000')
-        # The blog page's title contains Django blog
+        # The blog page's title and header contains Django blog
         self.assertIn('Django blog', self.browser.title)
+        self.assertIn('Django blog', 
+            self.browser.find_element_by_tag_name('h1').text
+        )
+
+        # She sees a link to the form for creating new blog posts
+        self.assertTrue(
+            self.browser.find_element_by_link_text(
+                '+ New Blog Post'
+            )
+            .is_displayed()
+        )
 
         # She enters the form to create a new blog post
+        self.browser.find_element_by_link_text(
+            '+ New Blog Post'
+        ).click()
 
+        # She enters a blog post text
+        inputbox = self.browser.find_element_by_ID('id_title')
+        inputbox.send_keys("Just Another Blog Post")
+        scrolldown = self.browser.find_element_by_name('')
+        Select(scrolldown).select_by_value('1')
+        
         # She submits the form
+        inputbox.find_element_by_xpath("//input[@value='Submit']").click()
 
         # She sees the new post on the main page
+
 
         # She sees the post details
 
